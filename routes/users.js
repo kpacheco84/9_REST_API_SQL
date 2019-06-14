47aa44f4ca6038eb6576ac3bee3049b3b503aeb5
kpacheco84 <katherinepacheco001@gmail.com>
2019-06-14 09:37:39 -0400

    post user not working
    post user not working

--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,50 @@ router.get("/", authenticate, (req, res) => {
 });
 
   
+// POST Create User
+router.post("/", (req, res, next) => {
+	const info = req.body;
+	//If email address has not been entered
+	if (!info.emailAddress) {
+		const err = new Error('You have not entered sufficient credentials');
+		err.status = 400;
+		next(err);
+	} else {
+		User.findOne({
+			where: {
+				emailAddress: info.emailAddress
+			}
+		}).then(email => {
+			//If email already exists
+			if (email) {
+				const err = new Error('That email address is already in use');
+				err.status = 400;
+				next(err);
+			} else {
+				//If Email Is Valid
+				//Hash Password
+				info.password = bcryptjs.hashSync(info.password);
+				//Create user
+				User.create(info).then(() => {
+						res.location('/');
+						res.status(201).end();
+					})
+					//Catch error and check if Sequelize validation  error 
+					.catch(err => {
+						if (err.name === "SequelizeValidationError") {
+							err.message = "All data must be entered";
+							err.status = 400;
+							next(err);
+						} else {
+							err.status = 400;
+							next(err);
+						}
+					});
+			}
+		});
+	}
+});
 
-  
 
 
 
